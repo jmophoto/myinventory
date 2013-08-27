db7cc14155fc8e00d98674e4cc8329ff212a1cc9
Jason Olson <jason@jmophoto.com>
2013-08-27 10:01:27 -0600

    Added page titles.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Inspeckd::Application.routes.draw do
+  devise_for :users
   get "static_pages/home"
   get "static_pages/about"
   get "static_pages/signup"
   get "static_pages/contact"
+  
+  devise_scope :user do
+    get "sign_in", :to => "devise/sessions#new"
+    get "sign_up", :to => "devise/registrations#new"
+  end
 
   root 'static_pages#home'
 end
