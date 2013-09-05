d8e6192f228fd11b1c6f2ea597870bd93d2429cb
Jason Olson <jason@jmophoto.com>
2013-09-05 17:43:51 -0600

    grouped features into categories.

--- app/models/room.rb
+++ app/models/room.rb
@@ -17,33 +17,32 @@ class Room < ActiveRecord::Base
   def add_features
     self.features = base_features
     if room_type == "bedroom"
-      self.features << bedroom_features
+      self.features.merge!(bedroom_features)
     elsif room_type == "bathroom"
-      self.features << bathroom_features
+      self.features.merge!(bathroom_features)
     elsif room_type == "kitchen"
-      self.features << kitchen_features
+      self.features.merge!(kitchen_features)
     end
-    self.features.flatten!
   end
   
   def base_features
-    ['walls', 'baseboards', 'doors', 'floors', 'ceiling', 'light fixtures']
+    { "base" => ['walls', 'baseboards', 'doors', 'floors', 'ceiling', 'light fixtures'] }
   end
   
   def bedroom_features
-    ['windows', 'screens', 'blinds/drapes', 'closets/shelves'] + window_features
+    { "bedroom" => ['windows', 'screens', 'blinds/drapes', 'closets/shelves'] }
   end
   
   def bathroom_features
-    ['tub/shower', 'toilet', 'countertop', 'sink', 'cabinets/shelves', 'mirrors', 'fan']
+    { "bathroom" => ['tub/shower', 'toilet', 'countertop', 'sink', 'cabinets/shelves', 'mirrors', 'fan'] }
   end
   
   def kitchen_features
-    ['fridge', 'range/oven', 'dishwasher', 'disposal', 'countertop', 'sink', 'cabinets/shelves'] + window_features
+    { "kitchen" => ['fridge', 'range/oven', 'dishwasher', 'disposal', 'countertop', 'sink', 'cabinets/shelves'] }
   end
   
   def window_features
-    ['windows', 'screens', 'blinds/drapes']
+    { "kitchen" => ['windows', 'screens', 'blinds/drapes'] }
   end
     
 end
