04b03a3df390ee7b12603b635216b81d785aeba8
Jason Olson <jason@jmophoto.com>
2013-10-14 10:31:52 -0600

    basic upload.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -69,6 +69,6 @@ class ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params.require(:image).permit(:inspection_id, :user_id)
+      params.require(:image).permit(:inspection_id, :user_id, :asset)
     end
 end
