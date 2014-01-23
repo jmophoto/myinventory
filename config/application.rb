f64e2be9783da379a16ca1626ebe90cf5ae010af
jmophoto <jason@jmophoto.com>
2014-01-23 09:49:52 -0700

    took out precomplie in application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Inspeckd
   class Application < Rails::Application
     
     #for bootstrap sass
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    # config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
