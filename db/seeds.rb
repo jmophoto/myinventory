7ca8578276a8afe36ff85bb005090c779f3372da
jmophoto <jason@jmophoto.com>
2014-03-23 21:39:40 -0600

    Add get started text, expanded homepage why, change update supscription to select plan, remove compnay info on profile page, rmeoved six icons on about page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,28 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Plan.create([
-  { plan_id: '10',
-    description: "$10/month for 5 inspections", 
-    price: 10, 
-    inspections_per_month: 5},
-  { plan_id: '19',
-    description: "$19/month for 10 inspections", 
-    price: 19, 
-    inspections_per_month: 10},
-  { plan_id: '35',
-    description: "$35/month for 20 inspections", 
-    price: 35, 
-    inspections_per_month: 20},
-  { plan_id: '50',
-    description: "$50/month for 50 inspections", 
-    price: 50, 
-    inspections_per_month: 50},
-  { plan_id: '75',
-    description: "$75/month for 100 inspections", 
-    price: 75, 
-    inspections_per_month: 100},
-  { plan_id: '99',
-    description: "$99/month for unlimited Inspections", 
-    price: 99, 
-    inspections_per_month: 9999},
+  { plan_id: 'agent',
+    description: "MI Agent completed inspections", 
+    price: 10
+  },
+  { plan_id: 'self',
+    description: "Self completed completed inspections", 
+    price: 10
+  }
 ])
