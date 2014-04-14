9ddc345de7bb7c4766ef685603ec461fff3932ae
Tim <thogg4@gmail.com>
2014-04-14 00:11:43 -0600

    admin users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,4 +8,19 @@ class Admin::UsersController < Admin::AdminController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to admin_user_path(@user), notice: 'User Updated'
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit!
+  end
+
 end
