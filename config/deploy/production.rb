cc3938ef1cd5a6adf8e587a0f520730d185935dc
jmophoto <jason@jmophoto.com>
2014-05-03 16:49:10 -0600

    new droplet from master

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{107.170.6.91}
-role :web, %w{107.170.6.91}
-role :db,  %w{107.170.6.91}
+role :app, %w{107.170.145.26}
+role :web, %w{107.170.145.26}
+role :db,  %w{107.170.145.26}
 
 # Extended Server Syntax
 # ======================
@@ -14,7 +14,7 @@ role :db,  %w{107.170.6.91}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '107.170.6.91', user: 'root', roles: %w{web app db}, my_property: :my_value
+server '107.170.145.26', user: 'root', roles: %w{web app db}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
